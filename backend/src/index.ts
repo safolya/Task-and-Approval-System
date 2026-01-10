2c249dcc1cf1d77386c089decf5504536dc2804f
Safolya Mondal <safolyamondal12345@gmail.com>
2026-01-10 16:17:14 +0530

    rate-limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,16 @@
 import express from "express"
-import userSchema from "./models/userSchema"
 import connectToDatabase from "./config/dbConnection";
 import dotenv from "dotenv"
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
 import cookieParser from "cookie-parser";
-import { authMiddle } from "./middlewares/authMiddleware";
-import { roleMiddle } from "./middlewares/roleMiddleware";
-import taskSchema from "./models/taskSchema";
-import teamSchema from "./models/teamSchema"
-import teamInvite from "./models/teamInvite";
-import teamMember from "./models/teamMember";
-import approvalSchema from "./models/approvalSchema";
-import notificationSchema from "./models/notificationSchema";
-import crypto from "crypto";
-import { managerMiddle } from "./middlewares/managerMiddleware";
-import mongoose from "mongoose";
-import createAuditLog from "./utils/auditLog";
 import authRoute from "./modules/auth/auth.routes"
 import teamRoute from "./modules/team/team.routes"
 import taskRoute from "./modules/task/task.routes"
 import approvalRoute from "./modules/approval/approval.routes"
+import limiter from "./service/rate-limit.service";
+
 const app = express();
 app.use(express.json())
+app.use(limiter)
 connectToDatabase()
 dotenv.config()
 
